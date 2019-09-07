155a9a18ae9af7b91d62c4810ca0dad9369778e2
bcalvario <bcalvario@ciencias.unam.mx>
2019-09-07 10:17:37 -0500

    Solve Me First

---
+++ solvemefirst.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int a = 3; //Declaracion de una varible tipo entero llamada a inicializada en 3
+    int b = 2; //Declaracion de una varible tipo entero llamada b inicializada en 2
+
+    cout << (a + b) << endl; //Se imprime la suma de a y b
+
+    return 0;
+}
