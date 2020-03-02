9b717b3644f610c88c36a5a403035f3a98ccc809
bcalvario <bcalvario@ciencias.unam.mx>
2020-03-02 00:10:15 -0600

    Update simplearraysum.cpp

--- simplearraysum.cpp
+++ simplearraysum.cpp
@@ -3,24 +3,24 @@
 using namespace std;
 
 int main(){
-    int n; //Se declara una variable de tipo entero llamada n
-    cin >> n; //Se le asigna a la varible n un valor por teclado
+    int n;
+    cin >> n;
 
-    int arr[n]; //Se declara una arreglo de enteros
+    int arr[n];
 
-    for (int i = 0; i < n; i++){ //For para recorrer el arreglo
+    for (int i = 0; i < n; i++) {
        
-        cin >> arr[i]; //Se lee los valores del arreglo por teclado
+        cin >> arr[i];
     }
 
-    int suma = 0; //Se declara una variabñe de tipo entero llamada suma y la inicializamos en cero
+    int suma = 0;
 
-    for (int i = 0; i < n; i++){ //Usamos un for para recorrer nuevamente el arreglo
+    for (int i = 0; i < n; i++) {
        
-        suma += arr[i]; //A suma se le asigna la suma de los valores del arreglo
+        suma += arr[i];
     }
 
-    cout << suma << endl; //Imprimimos las variable suma
+    cout << suma << endl;
 
     return 0;
 }
