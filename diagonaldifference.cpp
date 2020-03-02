3d671cf7f78e5a9f1999cb67719a1647f1cc5875
bcalvario <bcalvario@ciencias.unam.mx>
2020-03-02 00:08:45 -0600

    Update diagonaldifference.cpp

--- diagonaldifference.cpp
+++ diagonaldifference.cpp
@@ -4,47 +4,47 @@ using namespace std;
 
 int main(){
 
-    int n; //Declaramos una variable de tipo entero llamada n
+    int n;
 
-    cin >> n; //Le asignamos un valor a n leido desde teclado
+    cin >> n;
 
-    int arr[n][n]; //Declaramos una matriz
+    int arr[n][n];
 
-    for (int i = 0; i < n; i++){ //Usamos un for para recorrer la matriz por i
+    for (int i = 0; i < n; i++) {
 
-        for(int j = 0; j < n; j++){ //Usamos un for para recorrer la matriz por j
+        for(int j = 0; j < n; j++) {
 
-            cin >> arr[i][j]; //Asignamos al arreglo los valores leidos por teclado
+            cin >> arr[i][j];
 
         }
     }
 
-    int dd = 0; //Declaramos una varible de tipo entero llamada dd (diagonal derecha) inicializada con cero
-    int di = 0; //Declaramos una varible de tipo entero llamada di (diagonal izquierda) inicializada con cero
+    int dd = 0;
+    int di = 0;
 
-    for(int i = 0; i < n; i++){ //Con un for recorremos la matriz por i
+    for(int i = 0; i < n; i++) {
 
-        for(int j = 0; j < n; j++){ //Con un for recorremos la matriz por j al mismo tiempo que por i
+        for(int j = 0; j < n; j++) {
             
-            if(i == j){ //Usamos el condicional para indicar que si al recorrer la matriz i y j son iguales
+            if(i == j) {
 
-                dd += arr[i][j]; //Asignarle a dd (diagonal derecha) la suma de i y j cuando son iguales
+                dd += arr[i][j];
 
             }
         }
     }
 
-    int aux = n-1; //Declaramos una variable de tipo entero llamada aux (auxiliar) a la cual le asignamos n menos uno
+    int aux = n - 1;
 
-    for(int i = 0; i < n; i++){ //Usamoes un for para recorrer el arreglo por i
+    for(int i = 0; i < n; i++) {
 
-        di += arr[i][aux--]; //A la varible di (diagonal izquierda) le asignamos la suma del recorrido del la matriz por i y por aux--
+        di += arr[i][aux--];
 
     }
 
-    int difdd = abs(dd - di); //Declaramos una varible de tipo entero llamada difdd (diferencia de diagonales) y le asignamos el valor absoluto de la resta de dd (diagonal derecha) con di (diagonal izquierda)
+    int difdd = abs(dd - di);
 
-    cout << difdd <<endl; //Imprimimos la varible difdd (diferencia de diagonales)
+    cout << difdd << endl;
 
     return 0;
 }
